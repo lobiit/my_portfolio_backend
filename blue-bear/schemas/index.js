2fba060f81cee6e26508522da41738e170a95527
Tumbo Lobi John <johnlobiit@gmail.com>
2023-04-10 10:25:34 +0300

    created the work experience schema and registered it

--- blue-bear/schemas/index.js
+++ blue-bear/schemas/index.js
@@ -3,4 +3,6 @@ import abouts from "./abouts";
 import brands from "./brands";
 import contact from "./contact";
 import experiences from "./experiences";
-export const schemaTypes = [testimonials, abouts, brands, contact, experiences]
+import skills from "./skills";
+import workExperience from "./workExperience";
+export const schemaTypes = [testimonials, abouts, brands, contact, experiences, skills, workExperience]
