b8da2473932534db1e4ca5add8d37436fb7f34a9
Tumbo Lobi John <johnlobiit@gmail.com>
2023-04-10 10:22:20 +0300

    created the experiences schema and registered it

--- blue-bear/schemas/index.js
+++ blue-bear/schemas/index.js
@@ -2,4 +2,5 @@ import testimonials from "./testimonials";
 import abouts from "./abouts";
 import brands from "./brands";
 import contact from "./contact";
-export const schemaTypes = [testimonials, abouts, brands, contact]
+import experiences from "./experiences";
+export const schemaTypes = [testimonials, abouts, brands, contact, experiences]
